3434f8a257ab8731a0b4f3453ac1c099baf09c6f
n1kolay-zhurkin <zhurkun96@gmail.com>
2026-02-04 01:48:46 +0300

    Очистка main — удаляем весь код

--- pages/dzen_page.py
+++
@@ -1,9 +0,0 @@
-class DzenPage:
-    def __init__(self, driver):
-        self.driver = driver
-
-    def get_title(self):
-        return self.driver.title
-
-    def get_url(self):
-        return self.driver.current_url
